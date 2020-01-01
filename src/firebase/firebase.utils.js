f81bef989c64c9869a203c1e87ca57fb2cabb9a4
Jonathan Hsu <bit172@gmail.com>
2019-12-31 22:37:42 -0500

    Added user creation for Google OAuth to DB and setting state in application

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -13,6 +13,35 @@ const config = {
   measurementId: "G-PLBYN97MJW"
 };
 
+// async for api calls
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+  if (!userAuth) return;
+
+  const userRef = firestore.doc(`users/${userAuth.uid}`);
+
+  // Call Firestore for user
+  const snapShot = await userRef.get();
+
+  // Add user to Firestore if the user does not exist
+  if (!snapShot.exists) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+
+    try {
+      await userRef.set({
+        displayName,
+        email,
+        createdAt,
+        ...additionalData
+      });
+    } catch (error) {
+      console.log("error creating user", error.message);
+    }
+  }
+
+  return userRef;
+};
+
 firebase.initializeApp(config);
 
 export const auth = firebase.auth();
