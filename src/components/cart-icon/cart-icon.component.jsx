987131626997c1d23e3024c81b5caf8e7d270c31
Jonathan Hsu <bit172@gmail.com>
2020-01-04 00:07:14 -0500

    Replaced old selectors with createStructuredSelector for scalibility

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { connect } from "react-redux";
+import { createStructuredSelector } from "reselect";
 
 import { toggleCartHidden } from "../../redux/cart/cart.actions";
 import { selectCartItemsCount } from "../../redux/cart/cart.selectors";
@@ -32,8 +33,8 @@ const mapDispatchToProps = dispatch => ({
 // });
 
 // Memoized value of itemCount through Reselector
-const mapStateToProps = state => ({
-  itemCount: selectCartItemsCount(state)
+const mapStateToProps = createStructuredSelector({
+  itemCount: selectCartItemsCount
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(CartIcon);
